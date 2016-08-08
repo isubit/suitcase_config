cc62754b731cb9efcb78bb0b534fe52015296b3a
rlfrahm <frahmryan@gmail.com>
2016-08-08 16:52:25 -0500

    LUGG-763 : HTML in the template

--- templates/page--admin--config--system--site_information.tpl.php
+++ templates/page--admin--config--system--site_information.tpl.php
@@ -1,1 +1,43 @@
-<h1>Suitcase config override</h1>
+<?php
+/**
+ * @file
+ * Alpha's theme implementation to display a single Drupal page.
+ */
+?>
+<div<?php print $attributes; ?>>
+  <h1>Hi</h1>
+  <div class="container-12">
+  	<div class="grid-6 vertical-tabs clearfix">
+  		<ul class="vertical-tabs-list">
+  			<li class="vertical-tab-button">
+  				<a href="#">
+  					<strong>Thing 1</strong>
+  				</a>
+  			</li>
+  			<li class="vertical-tab-button">
+  				<a href="#">
+  					<strong>Thing 1</strong>
+  				</a>
+  			</li>
+  			<li class="vertical-tab-button">
+  				<a href="#">
+  					<strong>Thing 1</strong>
+  				</a>
+  			</li>
+  			<li class="vertical-tab-button">
+  				<a href="#">
+  					<strong>Thing 1</strong>
+  				</a>
+  			</li>
+  			<li class="vertical-tab-button">
+  				<a href="#">
+  					<strong>Thing 1</strong>
+  				</a>
+  			</li>
+  		</ul>
+  	</div>
+  	<div class="grid-6">
+  		
+  	</div>
+  </div>
+</div>
