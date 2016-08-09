476462f82969e0b5f1a0cd51378b60748fe212de
rlfrahm <frahmryan@gmail.com>
2016-08-09 08:59:40 -0500

    LUGG-763 : Style updates, live preview

--- templates/page--admin--config--system--site_information.tpl.php
+++ templates/page--admin--config--system--site_information.tpl.php
@@ -4,40 +4,167 @@
  * Alpha's theme implementation to display a single Drupal page.
  */
 ?>
+<style>
+  .vertical-tabs-list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    border: 1px solid #333;
+    border-radius: 4px;
+  }
+
+  .vertical-tab-button {
+    background-color: #ddd;
+    padding: 1em;
+  }
+
+  .vertical-tab-button:not(:last-child) {
+    border-bottom: 1px solid #333;
+  }
+
+  .vertical-tab-button:first-child {
+    border-top-left-radius: 2px;
+    border-top-right-radius: 2px;
+  }
+
+  .vertical-tab-button:last-child {
+    border-bottom-left-radius: 2px;
+    border-bottom-right-radius: 2px;
+  }
+
+  .vertical-tab-button.active,
+  .header-preview {
+    background-color: #cc0000;
+  }
+
+  .vertical-tab-button .form-text {
+    display: none;
+    font-size: 14px;
+    padding: 5px;
+    border-radius: 2px;
+    min-width: 300px;
+  }
+
+  .vertical-tab-button.active .form-text {
+    display: block;
+  }
+
+  .vertical-tab-button.active .field-name {
+    display: none;
+  }
+
+  .vertical-tab-button .field-container:not(:last-child) {
+    margin-bottom: 1em;
+  }
+
+  .container-12 .grid-6 {
+    width: 50%;
+    float: left;
+    padding: 1em;
+  }
+
+  .header-preview {
+    padding: 1em;
+    color: #fff;
+  }
+
+  .header-preview .site-name-level-1 {
+    margin-bottom: 0;
+    font-weight: 500;
+    font-size: 18px;
+    text-decoration: none;
+    line-height: 16px;
+    padding-top: 7px;
+    font-family: Helvetica neue, sans-serif;
+    letter-spacing: 0;
+    margin-top: 0;
+  }
+</style>
 <div<?php print $attributes; ?>>
-  <h1>Hi</h1>
-  <div class="container-12">
+  <div class="container-12 clearfix">
   	<div class="grid-6 vertical-tabs clearfix">
   		<ul class="vertical-tabs-list">
-  			<li class="vertical-tab-button">
-  				<a href="#">
-  					<strong>Thing 1</strong>
-  				</a>
+  			<li class="vertical-tab-button active" data-img="true" data-dname="true" data-lname="true">
+  				<div>
+            <img src="/suitcase_luggage/sites/all/themes/suitcase_interim/images/sprite.png" height="24px">
+            <div class="field-container field-department-name">
+              <span class="field-name">Department of Examples</span>
+              <input type="text" name="department-name" class="form-text" value="Department of Examples" placeholder="Enter Department Name">
+            </div>
+            <div class="field-container field-laboratory-name">
+              <span class="field-name">Laboratory of Jane Doe</span>
+              <input type="text" name="laboratory-name" class="form-text" value="Laboratory of Jane Doe" placeholder="Enter Laboratory Name">
+            </div>
+  				</div>
   			</li>
-  			<li class="vertical-tab-button">
-  				<a href="#">
-  					<strong>Thing 1</strong>
-  				</a>
+  			<li class="vertical-tab-button" data-img="true" data-dname="true" data-lname="false">
+          <div>
+            <img src="/suitcase_luggage/sites/all/themes/suitcase_interim/images/sprite.png" height="24px">
+            <div class="field-container field-department-name">
+              <span class="field-name">Department of Examples</span>
+              <input type="text" name="department-name" class="form-text" value="Department of Examples" placeholder="Enter Department Name">
+            </div>
+          </div>
   			</li>
-  			<li class="vertical-tab-button">
-  				<a href="#">
-  					<strong>Thing 1</strong>
-  				</a>
+  			<li class="vertical-tab-button" data-img="true" data-dname="false" data-lname="true">
+          <div>
+            <img src="/suitcase_luggage/sites/all/themes/suitcase_interim/images/sprite.png" height="24px">
+            <div class="field-container field-laboratory-name">
+              <span class="field-name">Laboratory of Jane Doe</span>
+              <input type="text" name="laboratory-name" class="form-text" value="Laboratory of Jane Doe" placeholder="Enter Laboratory Name">
+            </div>
+          </div>
   			</li>
-  			<li class="vertical-tab-button">
-  				<a href="#">
-  					<strong>Thing 1</strong>
-  				</a>
-  			</li>
-  			<li class="vertical-tab-button">
-  				<a href="#">
-  					<strong>Thing 1</strong>
-  				</a>
+  			<li class="vertical-tab-button" data-img="true" data-dname="false" data-lname="true">
+          <div>
+            <img src="/suitcase_luggage/sites/all/themes/suitcase_interim/images/sprite.png" height="24px">
+            <div class="field-container field-laboratory-name">
+              <span class="field-name">Laboratory of Jane Doe</span>
+              <input type="text" name="laboratory-name" class="form-text" value="Laboratory of Jane Doe" placeholder="Enter Laboratory Name">
+            </div>
+          </div>
   			</li>
   		</ul>
   	</div>
   	<div class="grid-6">
-  		
+      <div class="header-preview">
+    		<img src="/suitcase_luggage/sites/all/themes/suitcase_interim/images/sprite.png" height="24px" class="header-img">
+        <header class="header-text">
+          <h1 class="site-name-level-1">Department of Examples</h1>
+          <h2 class="site-name-level-2">Laboratory of Jane Doe</h2>
+        </header>
+      </div>
   	</div>
   </div>
+  <script type="text/javascript">
+    (function($) {
+      var $headerImg = $('.header-preview .header-img'),
+        $headerLevel1 = $('.header-preview .site-name-level-1'),
+        $headerLevel2 = $('.header-preview .site-name-level-2');
+
+      $('.vertical-tab-button').click(function() {
+        $('.vertical-tab-button').removeClass('active');
+        $(this).toggleClass('active');
+        var d = $(this).data();
+        console.log(d);
+        if (d.img) $headerImg.show();
+        else $headerImg.hide();
+        if (d.dname) $headerLevel1.show();
+        else $headerLevel1.hide();
+        if (d.lname) $headerLevel2.show();
+        else $headerLevel2.hide();
+      });
+
+      $('.field-department-name .form-text').bind("propertychange change click keyup input paste", function() {
+        $headerLevel1.text($(this).val());
+      });
+
+      $('.field-laboratory-name .form-text').bind("propertychange change click keyup input paste", function() {
+        $headerLevel2.text($(this).val());
+      });
+    })(jQuery)
+  </script>
+  <?php if (isset($page['content'])) : ?>
+    <?php print render($page['content']); ?>
+  <?php endif; ?>
 </div>
